a8823f849a53a46695f946c1a208456a36a538e4
Rosane <rosanemaia3108@gmail.com>
2025-04-09 16:31:06 -0300

    test-fatorial

---
+++ test/test_funcao_fatorial.py
@@ -0,0 +1,10 @@
+def teste_fatorial():
+    # Testes básicos
+    assert fatorial(0) == 1, "Erro: fatorial(0) deveria ser 1"
+    assert fatorial(1) == 1, "Erro: fatorial(1) deveria ser 1"
+    assert fatorial(2) == 2, "Erro: fatorial(2) deveria ser 2"
+    assert fatorial(3) == 6, "Erro: fatorial(3) deveria ser 6"
+    assert fatorial(4) == 24, "Erro: fatorial(4) deveria ser 24"
+    assert fatorial(5) == 120, "Erro: fatorial(5) deveria ser 120"
+    
+    
